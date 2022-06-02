97bd0e16cc68194aea6b26a66292d2041b4621a6
Student <Student@STUDENT-250JE01>
2022-06-02 15:10:55 +0200

    Initial Upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Student
+ *
+ * Created on June 2, 2022, 3:09 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
